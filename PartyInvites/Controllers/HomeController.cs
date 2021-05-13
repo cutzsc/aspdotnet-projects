a0a9d2656cf4a59374e972f924417d3a0d4c93bd
Ildar Tagiev <tag.ildar@gmail.com>
2021-05-13 12:50:44 +0300

    chapter 3, party invites project

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -9,29 +9,33 @@ using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		[HttpGet]
+		public IActionResult RsvpForm()
+		{
+			return View();
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		[HttpPost]
+		public IActionResult RsvpForm(GuestResponse guestResponse)
+		{
+			if (ModelState.IsValid)
+			{
+				Repository.AddResponse(guestResponse);
+				return View("Thanks", guestResponse);
+			}
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult ListResponses()
+		{
+			return View(Repository.Responses.Where(item => item.WillAttend == true));
+		}
+	}
 }
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+	public static class Repository
+	{
+		private static List<GuestResponse> guestResponses = new List<GuestResponse>();
+
+		public static IEnumerable<GuestResponse> Responses => guestResponses;
+
+		public static void AddResponse(GuestResponse guestResponse) => guestResponses.Add(guestResponse);
+	}
+}
